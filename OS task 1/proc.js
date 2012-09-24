0d6f7a97866cec49b52df2d2daaf3e36969f7ff8
Andrey Popov <andrey--popov@yandex.ru>
2012-09-24 23:26:48 +0600

    add regcom

--- OS task 1/proc.js
+++ OS task 1/proc.js
@@ -1,5 +1,4 @@
-
-//INSTRUCTION POINTER - указатель команды. Хранит адрес текущей команды. Изначально равен 0.
+//INSTRUCTION POINTER - указатель команды. Хранит адрес текущей команды. Изначально равен 0. Предоставляет доступ к адресу текущей команды
 function INSTRUCTION_POINTER(value) {
 	this.value = value;
 	this.getValue = function() {
@@ -8,12 +7,15 @@ function INSTRUCTION_POINTER(value) {
 }
 var ip = new INSTRUCTION_POINTER(0);
 
-//RANDOM ACCESS MEMORY - оперативная память. 256 ячеек по 4 бита каждая. Изнициализирована содержимым файла "memory.txt"
+//RANDOM ACCESS MEMORY - оперативная память. 256 ячеек по 4 бита каждая. Изнициализирована содержимым файла "memory.txt". Предоставляет доступ к указанной ячейке памяти
 function RANDOM_ACCESS_MEMORY(init) {
 	this.content = new Array(256);
 	this.content = init;
 	this.getContent = function(index) {
 		return(this.content[index]);
+	}
+	this.readCommand = function(index) {
+		return((this.content[index]) + "" + (this.content[index + 1]) + "" + (this.content[index + 2]) + "" + (this.content[index + 3]));
 	}
 }
 var fso = new ActiveXObject("Scripting.FileSystemObject");
@@ -22,9 +24,27 @@ var file = fso.OpenTextFile(file_name, 1, true);
 var memory = new RANDOM_ACCESS_MEMORY(file.ReadAll().replace(/[\t\r\n ]/g, "").split(""));
 file.Close();
 
+//REGCOM - хранит команду целиком. Возвращает отдельно код операции и адресную часть команды
+function REGCOM() {
+	this.setCommand = function(command) {
+		this.operation_code = command.substr(0,2);
+		this.address = command.substr(2,2);
+	}
+	this.getOpCode = function() {
+		return this.operation_code;
+	}
+	this.getAddress = function() {
+		return this.address
+	}	
+}
+var regcom = new REGCOM();
+
+
+regcom.setCommand(memory.readCommand(ip.getValue()));
 
 
 
 
 
-WSH.echo(memory.getContent(0));
+
+WSH.echo(regcom.getOpCode());
