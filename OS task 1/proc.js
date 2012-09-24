69e7c8218f77dc4aacf45bb5ed81e9b5fea9ceec
Andrey Popov <andrey--popov@yandex.ru>
2012-09-24 22:54:47 +0600

    transfering to objects model

--- OS task 1/proc.js
+++ OS task 1/proc.js
@@ -1,11 +1,30 @@
-//»нициализируем MEMORY (оперативную пам€ть) содержимым файла "memory.txt", игнориру€ пробелы
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-var file_name = "memory.txt";
-var file = fso.OpenTextFile(file_name,1,true);
-var file_content = file.ReadAll();
-file.Close();
-var MEMORY = new Array(256);
-MEMORY = file_content.replace(/[\t\r\n ]/g, "").split("");
 
 //INSTRUCTION POINTER - указатель команды. ’ранит адрес текущей команды. »значально равен 0.
-var IP = 0;
+function INSTRUCTION_POINTER(value) {
+	this.value = value;
+	this.getValue = function() {
+		return(this.value);
+	}
+}
+var ip = new INSTRUCTION_POINTER(0);
+
+//RANDOM ACCESS MEMORY - оперативна€ пам€ть. 256 €чеек по 4 бита кажда€. »знициализирована содержимым файла "memory.txt"
+function RANDOM_ACCESS_MEMORY(init) {
+	this.content = new Array(256);
+	this.content = init;
+	this.getContent = function(index) {
+		return(this.content[index]);
+	}
+}
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var file_name = "memory.txt";
+var file = fso.OpenTextFile(file_name, 1, true);
+var memory = new RANDOM_ACCESS_MEMORY(file.ReadAll().replace(/[\t\r\n ]/g, "").split(""));
+file.Close();
+
+
+
+
+
+
+WSH.echo(memory.getContent(0));
