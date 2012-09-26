c7ef856cf2dde94f47d79e550487f408fec419d1
Andrey Popov <andrey--popov@yandex.ru>
2012-09-26 18:30:16 +0600

    deccom and env

--- OS task 1/proc.js
+++ OS task 1/proc.js
@@ -36,6 +36,8 @@ file.Close();
 
 //REGCOM - хранит команду целиком. Возвращает отдельно код операции и адресную часть команды
 function REGCOM() {
+	this.operation_code = 0;
+	this.address = 0;
 	this.setCommand = function(command) {
 		this.operation_code = command[0];
 		this.address = command[1];
@@ -51,6 +53,8 @@ var regcom = new REGCOM();
 
 //ADDER - устройство целочисленного сложения.
 function ADDER() {
+	this.first = 0;
+	this.second = 0;
 	this.setFirst = function(operand) {
 		this.first = operand;
 	}
@@ -63,10 +67,81 @@ function ADDER() {
 }
 var adder = new ADDER();
 
-//DECCOM
+//УПРАВЛЯЮЩИЕ СИГНАЛЫ - реализованны в виде абстрактного объекта, сочетающие набор переменных окружения
+function ENV() {
+
+	this.KOP = 0;	//выпилить
+	this.PR = 0x00;
+	this.F = false;
+	
+	this.I = 0;
+	this.P = 0;
+	this.OP = 0;	
+	this.PEREH = false;
+	
+	this.PYSK = true;
+	this.VZAP1 = false;
+	this.ZAM1 = false;
+	this.ZAM2 = false;
+	this.CHIST = false;
+	this.VIB = false;
+	this.ZAPP = false;
+	this.PEREH = false;
+}
+
+//DECCOM - подает сигналы на соответствующие устройства через ENV - переменные окружения
 function DECCOM() {
-	this.setParam = function(opCode) {
-		WSH.Echo(opCode);
+	this.setParam = function(opCode, env) {
+		switch (opCode) {
+			case 0x00:	env.P = 0;			//ЗАПИСЬ
+						env.OP = 0;
+						env.PEREH = false;
+						break;
+			case 0x11:	env.I = 0;			//СЧИТЫВАНИЕ
+						env.P = 1;
+						env.OP = 1;
+						env.PEREH = false;
+						break;
+			case 0x15:	env.I = 1;			//СЧИТЫВАНИЕ ИА
+						env.P = 1;
+						env.OP = 1;
+						env.PEREH = false;
+						break;
+			case 0x02:	env.P = 2;			//ЗАПИСЬ В ИР
+						env.OP = 0;
+						env.PEREH = false;
+						break;
+			case 0x21:	env.I = 0;			//СЛОЖЕНИЕ
+						env.P = 1;
+						env.OP = 2;
+						env.PEREH = false;
+						break;
+			case 0x25:	env.I = 1;			//СЛОЖЕНИЕ С ИА
+						env.P = 1;
+						env.OP = 2;
+						env.PEREH = false;
+						break;
+			case 0x31:	env.I = 0;			//ВЫЧИТАНИЕ
+						env.P = 1;
+						env.OP = 3;
+						env.PEREH = false;
+						break;
+			case 0xFE:	env.P = 4;			//БЕЗУСЛОВНЫЙ ПЕРЕХОД
+						env.OP = 0xF;
+						env.PEREH = true;
+						break;
+			case 0xFF:	env.P =4;			//ОСТАНОВ
+						env.OP = 0xF;
+						env.PEREH = false;
+						break;
+		}
+		env.ZAPP = (env.P == 0);
+		env.ZAM1 = (env.P == 1);
+		env.ZAM2 = (env.P != 3);
+		env.VZAP1 = (env.P == 3);
+		env.VIB = (env.I);
+		env.CHIST = !(env.P == 2 || env.P == 3);
+		env.PYSK = (opCode != 0xFF);
 	}
 }
 var deccom = new DECCOM();
