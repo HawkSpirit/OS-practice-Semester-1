cf3e07e55b0f9367221b7157ac7d25c9dc20d436
Andrey Popov <andrey--popov@yandex.ru>
2012-12-05 20:00:26 +0600

    cmpxchg really works!))

--- OS task 4/1.cpp
+++ OS task 4/1.cpp
@@ -8,7 +8,7 @@ int main()	{
 	uint32_t *ptr;
 	uint32_t ret_val,old_val,new_val;
 	
-	asm ("pusha\npopa\n" : "=a"(ret_val) : "r"(new_val),"m"(*ptr),"0"(old_val) : "memory");
+	asm ("pusha\ncmpxchg %eax, %eax\npopa\n");
 		
     cout << "SUCCESS!" << endl;
     return 0;
