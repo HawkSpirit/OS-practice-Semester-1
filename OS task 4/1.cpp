00dfe3e4707083dfe3393f5a70d15ff437e2612f
Andrey Popov <andrey--popov@yandex.ru>
2012-12-05 20:49:36 +0600

    YEEEAAHHH!!! IT WORKS!!!

--- OS task 4/1.cpp
+++ OS task 4/1.cpp
@@ -5,10 +5,22 @@ using namespace std;
  
 int main()	{
 
-	uint32_t *ptr;
-	uint32_t ret_val,old_val,new_val;
+	int old_val = 5;
+	int new_val = 6;
+	int *ptr = new int(1);	
+	ptr[0] = 5;
+
+	cout << old_val << endl;
+	cout << new_val << endl;
+	cout << *ptr << endl;
 	
-	asm ("pusha\ncmpxchg %eax, %eax\npopa\n");
+	cout << "--------CMPXCHG------------" << endl;		
+	
+	asm volatile("pusha\nmovl %0,%%eax\nlock\ncmpxchgl %1,%2\npopa\n" :: "r"(old_val), "r"(new_val), "m"(*ptr) : "memory");
+	
+	cout << old_val << endl;
+	cout << new_val << endl;
+	cout << *ptr << endl;
 		
     cout << "SUCCESS!" << endl;
     return 0;
