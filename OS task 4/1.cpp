68223bbcca9f2c4921d683a20212a1fe8af337e9
Andrey Popov <andrey--popov@yandex.ru>
2012-12-05 19:52:53 +0600

    ..

--- OS task 4/1.cpp
+++ OS task 4/1.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <string>
  
 using namespace std;
  
 int main()	{
+
+	uint32_t *ptr;
+	uint32_t ret_val,old_val,new_val;
 	
-	asm("pusha\npopa");
+	asm ("pusha\npopa\n" : "=a"(ret_val) : "r"(new_val),"m"(*ptr),"0"(old_val) : "memory");
 		
-    cout << "SUCCESS" << endl;
+    cout << "SUCCESS!" << endl;
     return 0;
 }
