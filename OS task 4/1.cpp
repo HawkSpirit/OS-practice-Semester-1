a9bceac7ba5a20ec62863ad450b9c1e77386e3c1
Andrey Popov <andrey--popov@yandex.ru>
2012-12-14 18:23:56 +0600

    proof of ncmpxchg correct operation 64 threads

--- OS task 4/1.cpp
+++ OS task 4/1.cpp
@@ -4,143 +4,123 @@
 #include <tchar.h>
 #include <strsafe.h>
 
-#define MAX_THREADS 3
-#define BUF_SIZE 255
+#define MAX_THREADS 64
 
-DWORD WINAPI MyThreadFunction( LPVOID lpParam );
+int counterSimple = 0;
+int counterProtected = 0;
+int flag = 0;
+
+DWORD WINAPI IncSimle(LPVOID lpParam);
+DWORD WINAPI IncProtected(LPVOID lpParam);
 void ErrorHandler(LPTSTR lpszFunction);
-
-typedef struct MyData {
-    int val1;
-    int val2;
-} MYDATA, *PMYDATA;
 
 using namespace std;
  
-int main()	{
-
+int main() {
 	int old_val = 5;
 	int new_val = 6;
 	int *ptr = new int(1);	
 	ptr[0] = 5;
 
-	cout << old_val << endl;
-	cout << new_val << endl;
-	cout << *ptr << endl;
+	cout << endl << "OLD VAL:   " << *ptr << endl;
+	cout << "NEW VAL:   " << new_val << endl;
 	
-	cout << "--------CMPXCHG------------" << endl;		
+	cout << "-----------------------CMPXCHG-----------------------" << endl;
 	
-	asm volatile("pusha\nmovl %0,%%eax\nlock\ncmpxchgl %1,%2\npopa\n" :: "r"(old_val), "r"(new_val), "m"(*ptr) : "memory");
+	asm volatile("pusha\n");
+	asm volatile("movl %0,%%eax\nlock\ncmpxchgl %1,%2\n" :: "r"(old_val), "r"(new_val), "m"(*ptr) : "memory");
+	asm volatile("popa\n");
 	
-	cout << old_val << endl;
-	cout << new_val << endl;
-	cout << *ptr << endl;
-		
-    cout << "SUCCESS!" << endl;
+	cout << "OLD VAL:   " << *ptr << endl;
+	cout << "NEW VAL:   " << new_val << endl << endl;
 	
-	    PMYDATA pDataArray[MAX_THREADS];
     DWORD   dwThreadIdArray[MAX_THREADS];
     HANDLE  hThreadArray[MAX_THREADS]; 
 
+	
+	counterSimple = 0;
     // Create MAX_THREADS worker threads.
+    for( int i=0; i<MAX_THREADS; i++ ) {
 
-    for( int i=0; i<MAX_THREADS; i++ )
-    {
-        // Allocate memory for thread data.
-
-        pDataArray[i] = (PMYDATA) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY,
-                sizeof(MYDATA));
-
-        if( pDataArray[i] == NULL )
-        {
-           // If the array allocation fails, the system is out of memory
-           // so there is no point in trying to print an error message.
-           // Just terminate execution.
-            ExitProcess(2);
-        }
-
-        // Generate unique data for each thread to work with.
-
-        pDataArray[i]->val1 = i;
-        pDataArray[i]->val2 = i+100;
-
-        // Create the thread to begin execution on its own.
-
-        hThreadArray[i] = CreateThread( 
-            NULL,                   // default security attributes
-            0,                      // use default stack size  
-            MyThreadFunction,       // thread function name
-            pDataArray[i],          // argument to thread function 
-            0,                      // use default creation flags 
-            &dwThreadIdArray[i]);   // returns the thread identifier 
-
+        // Create the thread to begin execution on its own
+        hThreadArray[i] = CreateThread(NULL, 0, IncSimle, NULL, 0, &dwThreadIdArray[i]);   // returns the thread identifier 
 
         // Check the return value for success.
         // If CreateThread fails, terminate execution. 
         // This will automatically clean up threads and memory. 
-
-        if (hThreadArray[i] == NULL) 
-        {
+        if (hThreadArray[i] == NULL) {
            ErrorHandler(TEXT("CreateThread"));
            ExitProcess(3);
         }
-    } // End of main thread creation loop.
+    }
 
     // Wait until all threads have terminated.
-
     WaitForMultipleObjects(MAX_THREADS, hThreadArray, TRUE, INFINITE);
 
     // Close all thread handles and free memory allocations.
-
-    for(int i=0; i<MAX_THREADS; i++)
-    {
+    for(int i=0; i<MAX_THREADS; i++) {
         CloseHandle(hThreadArray[i]);
-        if(pDataArray[i] != NULL)
-        {
-            HeapFree(GetProcessHeap(), 0, pDataArray[i]);
-            pDataArray[i] = NULL;    // Ensure address is not reused.
+    }
+	
+	
+	counterProtected = 0;	
+    // Create MAX_THREADS worker threads.
+    for( int i=0; i<MAX_THREADS; i++ ) {
+
+        // Create the thread to begin execution on its own
+        hThreadArray[i] = CreateThread(NULL, 0, IncProtected, NULL, 0, &dwThreadIdArray[i]);   // returns the thread identifier 
+
+        // Check the return value for success.
+        // If CreateThread fails, terminate execution. 
+        // This will automatically clean up threads and memory. 
+        if (hThreadArray[i] == NULL) {
+           ErrorHandler(TEXT("CreateThread"));
+           ExitProcess(3);
         }
     }
+
+    // Wait until all threads have terminated.
+    WaitForMultipleObjects(MAX_THREADS, hThreadArray, TRUE, INFINITE);
+
+    // Close all thread handles and free memory allocations.
+    for(int i=0; i<MAX_THREADS; i++) {
+        CloseHandle(hThreadArray[i]);
+    }
+	
+	cout << "-----------------------RESULTS-----------------------" << endl;
+	cout << "SIMPLE INCREMENT RESULT:      " << counterSimple << " (must be 640000)" << endl << endl;	
+	cout << "PROTECTED INCREMENT RESULT:   " << counterProtected << " (must be 640000)" << endl << endl;	
+	
+	cout << "----------------------TRY AGAIN----------------------" << endl;
 
     return 0;
 }
 
 
-DWORD WINAPI MyThreadFunction( LPVOID lpParam ) 
-{ 
-    HANDLE hStdout;
-    PMYDATA pDataArray;
+DWORD WINAPI IncSimle(LPVOID) {
+    for (int i = 0; i < 10000; ++i)
+    {
+        counterSimple += 1;
+    }
+}
 
-    TCHAR msgBuf[BUF_SIZE];
-    size_t cchStringSize;
-    DWORD dwChars;
+DWORD WINAPI IncProtected(LPVOID) { 
+	asm volatile("pusha\n");
+	for (int i = 0; i < 10000; ++i) {
+		asm volatile("movl $1, %edx\n");
+		asm volatile("looper:\n");
+        asm volatile("movl %0, %%ebx\n" :: "r" (&flag));
+		asm volatile("movl $0, %eax\n");
+        asm volatile("lock\ncmpxchgl %edx, (%ebx)\n");
+        asm volatile("jnz looper\n");
+        counterProtected += 1;
+		asm volatile("movl $0, (%0)\n" :: "r" (&flag));
+    }
+	asm volatile("popa\n");
+    return 0;
+}
 
-    // Make sure there is a console to receive output results. 
-
-    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-    if( hStdout == INVALID_HANDLE_VALUE )
-        return 1;
-
-    // Cast the parameter to the correct data type.
-    // The pointer is known to be valid because 
-    // it was checked for NULL before the thread was created.
- 
-    pDataArray = (PMYDATA)lpParam;
-
-    // Print the parameter values using thread-safe functions.
-
-    StringCchPrintf(msgBuf, BUF_SIZE, TEXT("Parameters = %d, %d\n"), 
-        pDataArray->val1, pDataArray->val2); 
-    StringCchLength(msgBuf, BUF_SIZE, &cchStringSize);
-    WriteConsole(hStdout, msgBuf, (DWORD)cchStringSize, &dwChars, NULL);
-
-    return 0; 
-} 
-
-
-
-void ErrorHandler(LPTSTR lpszFunction) 
-{ 
+void ErrorHandler(LPTSTR lpszFunction) { 
     // Retrieve the system error message for the last-error code.
 
     LPVOID lpMsgBuf;
@@ -159,12 +139,8 @@ void ErrorHandler(LPTSTR lpszFunction)
 
     // Display the error message.
 
-    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
-        (lstrlen((LPCTSTR) lpMsgBuf) + lstrlen((LPCTSTR) lpszFunction) + 40) * sizeof(TCHAR)); 
-    StringCchPrintf((LPTSTR)lpDisplayBuf, 
-        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
-        TEXT("%s failed with error %d: %s"), 
-        lpszFunction, dw, lpMsgBuf); 
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, (lstrlen((LPCTSTR) lpMsgBuf) + lstrlen((LPCTSTR) lpszFunction) + 40) * sizeof(TCHAR)); 
+    StringCchPrintf((LPTSTR)lpDisplayBuf, LocalSize(lpDisplayBuf) / sizeof(TCHAR), TEXT("%s failed with error %d: %s"), lpszFunction, dw, lpMsgBuf); 
     MessageBox(NULL, (LPCTSTR) lpDisplayBuf, TEXT("Error"), MB_OK); 
 
     // Free error-handling buffer allocations.
